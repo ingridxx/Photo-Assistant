99a53d3117e4d7a3c3ca9c93596e4664331a98c7
Beck Gillespie <beckjgillespie@gmail.com>
2019-09-25 10:22:16 +0800

    finishing off the adapters for the recycler views - beck

--- app/src/main/java/com/example/photoassistant/RecycleAdapterLens.java
+++ app/src/main/java/com/example/photoassistant/RecycleAdapterLens.java
@@ -30,9 +30,9 @@ public class RecycleAdapterLens extends androidx.recyclerview.widget.RecyclerVie
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         //binds the data to the view.
-        viewHolder.Title.setText(list_item.get(i).getPartName());
-        viewHolder.Author.setText("min : " + list_item.get(i).getMinZoom() + "max : " +  list_item.get(i).getMaxZoom());
-        viewHolder.Date.setText("Lens");
+        viewHolder.topLine.setText(list_item.get(i).getPartName());
+        viewHolder.middleLine.setText("min : " + list_item.get(i).getMinZoom() + "max : " +  list_item.get(i).getMaxZoom());
+        viewHolder.bottomLine.setText("Lens placeholder");
 
     }
 
@@ -42,15 +42,15 @@ public class RecycleAdapterLens extends androidx.recyclerview.widget.RecyclerVie
     }
 
     public class ViewHolder extends androidx.recyclerview.widget.RecyclerView.ViewHolder{
-        TextView Title;
-        TextView Author;
-        TextView Date;
+        TextView topLine;
+        TextView middleLine;
+        TextView bottomLine; // of the cardview layout
         LinearLayout Parent_Layout;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            Title = itemView.findViewById(R.id.titleLabel);
-            Author = itemView.findViewById(R.id.authorLabel);
-            Date = itemView.findViewById(R.id.dateLabel);
+            topLine = itemView.findViewById(R.id.titleLabel);
+            middleLine = itemView.findViewById(R.id.authorLabel);
+            bottomLine = itemView.findViewById(R.id.dateLabel);
             Parent_Layout = itemView.findViewById(R.id.parent_layout);
         }
     }
