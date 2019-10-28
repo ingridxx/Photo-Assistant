2fd0c74948eab28dd2b81290b3945b76af1e00e6
ryantora <treeko39@gmail.com>
2019-10-28 19:18:33 +0800

    Revert "camera and live parameters function"
    This reverts commit 8bc2f1a1e69660e0c477151a7fdb952e4c361b48.

--- app/src/main/java/com/example/photoassistant/Calculator.java
+++ app/src/main/java/com/example/photoassistant/Calculator.java
@@ -4,16 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.CaptureResult;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.StreamConfigurationMap;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -25,15 +15,10 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
-import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.Surface;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -45,12 +30,6 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 
 public class Calculator extends Fragment {
@@ -64,22 +43,10 @@ public class Calculator extends Fragment {
     Button bodyButton, lensButton;
     TextView evTextView;
 
-    private TextureView mTextureView;
     Activity activity;
-
-    private Size mPreviewSize;
-    private String mCameraId;
-
     public Calculator() {
 
     }
-
-    private HandlerThread mBackgroundThread;
-    private Handler mBackgroundHandler;
-    private TotalCaptureResult mCaptureResult;
-    Button button;
-    CameraCharacteristics mCameraCharacteristics;
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -94,36 +61,6 @@ public class Calculator extends Fragment {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_calculator, container, false);
         fab = rootView.findViewById(R.id.floatingActionButton);
-
-
-        button = (Button)rootView.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                createCameraPreviewSession();
-                //
-                // Toast.makeText(getApplicationContext(),mCaptureResult.get(TotalCaptureResult.LENS_FOCUS_DISTANCE).toString(), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        final Handler handler=new Handler();
-        handler.post(new Runnable(){
-            @Override
-            public void run() {
-                if(mCaptureResult!=null) {
-
-                    button.setText("ISO:"+mCaptureResult.get(TotalCaptureResult.SENSOR_SENSITIVITY).toString()+
-                            " SPD:"+mCaptureResult.get(TotalCaptureResult.SENSOR_EXPOSURE_TIME)+
-                            " DIST:"+0.61/mCaptureResult.get(TotalCaptureResult.LENS_FOCUS_DISTANCE)+
-                            " MM:"+mCaptureResult.get(TotalCaptureResult.LENS_FOCAL_LENGTH));
-                }
-                handler.postDelayed(this,250); // set time here to refresh textView
-            }
-        });
-
-        mTextureView = (TextureView) rootView.findViewById(R.id.textureView);
-
 
         return rootView;
     }
@@ -210,217 +147,6 @@ public class Calculator extends Fragment {
         bodyButton.setText(Intelligence.Current.getBody().getPartName());
         lensButton.setText(Intelligence.Current.getLens().getPartName());
         evTextView.setText(Double.toString(Intelligence.ExposureCalculator()));
-    }
-
-    private TextureView.SurfaceTextureListener mSurfaceTextureListener =
-            new TextureView.SurfaceTextureListener() {
-                @Override
-                public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-
-                    setupCamera(width, height);
-                    openCamera();
-                }
-
-                @Override
-                public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
-                }
-
-                @Override
-                public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-                    return false;
-                }
-
-                @Override
-                public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-
-                }
-            };
-    private CameraDevice mCameraDevice;
-    private CameraDevice.StateCallback mCameraDeviceStateCallBack
-            = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            mCameraDevice = camera;
-
-            createCameraPreviewSession();
-            // Toast.makeText(activity.getApplicationContext(), "Camera Opened!", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            camera.close();
-            mCameraDevice = null;
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            camera.close();
-            mCameraDevice = null;
-        }
-    };
-    private CaptureRequest mPreviewCaptureRequest;
-    private CaptureRequest.Builder mPreviewCaptureRequestBuilder;
-    private CameraCaptureSession mCameraCaptureSession;
-    private CameraCaptureSession.CaptureCallback mSessionCaptureCallback
-            = new CameraCaptureSession.CaptureCallback() {
-        @Override
-        public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
-            super.onCaptureStarted(session, request, timestamp, frameNumber);
-        }
-    };
-
-
-    private void setupCamera(int width, int height) {
-        CameraManager cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            for (String cameraId : cameraManager.getCameraIdList()) {
-                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
-                        CameraCharacteristics.LENS_FACING_FRONT) {
-                    continue;
-                }
-                StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-                mPreviewSize = getPreferredPreviewSize(map.getOutputSizes(SurfaceTexture.class), width, height);
-                mCameraId = cameraId;
-                return;
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Size getPreferredPreviewSize(Size[] mapSizes, int width, int height) {
-        List<Size> collectorSizes = new ArrayList<>();
-        for (Size option : mapSizes) {
-            if (width > height) {
-                if (option.getWidth() > width && option.getHeight() > height) {
-                    collectorSizes.add(option);
-                }
-            } else {
-                if (option.getWidth() > height &&
-                        option.getHeight() > width) {
-                    collectorSizes.add(option);
-                }
-            }
-        }
-        if (collectorSizes.size() > 0) {
-            return Collections.min(collectorSizes, new Comparator<Size>() {
-                @Override
-                public int compare(Size lhs, Size rhs) {
-                    return Long.signum(lhs.getWidth() * lhs.getHeight() - rhs.getWidth() * rhs.getHeight());
-                }
-            });
-
-        }
-        return mapSizes[0];
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        openBackgroundThread();
-
-        if (mTextureView.isAvailable()) {
-            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
-            openCamera();
-        } else {
-            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
-        }
-    }
-
-    @Override
-    public void onPause() {
-
-        closeCamera();
-        closeBackgroundThread();
-        super.onPause();
-    }
-
-
-    private void openCamera() {
-        CameraManager cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            cameraManager.openCamera(mCameraId, mCameraDeviceStateCallBack, mBackgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void closeCamera() {
-        if(mCameraCaptureSession != null) {
-            mCameraCaptureSession.close();
-            mCameraCaptureSession = null;
-        }
-        if (mCameraDevice != null){
-            mCameraDevice.close();
-            mCameraDevice = null;
-        }
-    }
-
-    public void createCameraPreviewSession() {
-        final SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
-        surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
-        Surface previewSurface = new Surface(surfaceTexture);
-        try {
-
-            mPreviewCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            mPreviewCaptureRequestBuilder.addTarget(previewSurface);
-            mPreviewCaptureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-            mPreviewCaptureRequestBuilder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, 0);
-            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession session) {
-                    if (mCameraDevice == null) {
-                        return;
-                    }
-                    try {
-                        session.setRepeatingRequest(mPreviewCaptureRequestBuilder.build(),  new CameraCaptureSession.CaptureCallback() {
-                            @Override
-                            public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
-                                mCaptureResult = result;
-                            }
-                        }, mBackgroundHandler);
-
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    //Toast.makeText(getApplicationContext(), "create camera session failed!",
-                    //    Toast.LENGTH_SHORT).show();
-                }
-            }, null);
-
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-    private void openBackgroundThread() {
-        mBackgroundThread = new HandlerThread("Camera2 background thread");
-        mBackgroundThread.start();
-        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
-    }
-
-    private void closeBackgroundThread() {
-        mBackgroundThread.quitSafely();
-        try{
-            mBackgroundThread.join();
-            mBackgroundThread = null;
-            mBackgroundHandler = null;
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public CaptureRequest.Builder createCaptureRequest(int template) throws CameraAccessException {
-        CameraDevice device = mCameraDevice;
-        if (device == null) {
-            throw new IllegalStateException("Can't get requests when no camera is open");
-        }
-        return device.createCaptureRequest(template);
     }
 
 }
